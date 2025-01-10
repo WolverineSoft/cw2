5dab5252e9de86e9f2f243dc60e541fc0eaa0396
Ravi Bhatt <rnbhatt@umich.edu>
2025-01-09 23:52:03 -0800

    initial restoration

---
+++ download/Crisis Wolverine Downloads_files/basic.js
@@ -0,0 +1,35 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+function nedstatbasic(id, options) {document.write('<sc'+'ript type="text/javascript" src="https://web.archive.org/web/20160710002800/http://www.nedstatbasic.net/basicredir?id=' + id + '"></scr'+'ipt>');}
+
+}
+/*
+     FILE ARCHIVED ON 00:28:00 Jul 10, 2016 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 05:57:57 Jan 10, 2025.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.884
+  exclusion.robots: 0.027
+  exclusion.robots.policy: 0.011
+  esindex: 0.014
+  cdx.remote: 15.089
+  LoadShardBlock: 1349.199 (6)
+  PetaboxLoader3.datanode: 472.742 (8)
+  PetaboxLoader3.resolve: 2233.056 (2)
+  load_resource: 1445.987 (2)
+*/
